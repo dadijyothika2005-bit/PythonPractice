97c9e1943b489bd4c04ff8f4d7742adada79247e
Dadi Jyothika <dadijyothika2005@gmail.com>
2026-02-01 20:31:56 +0530

    LC2460 using 2 pointer

--- LC2460.py
+++ LC2460.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def applyOperations(self, nums):
+
+def applyOperations(nums):
         k=0
         for i in range(len(nums)-1):
             if(nums[i]==nums[i+1]):
@@ -12,3 +12,5 @@ class Solution(object):
         for i in range(k,len(nums)):
             nums[i]=0       
         return nums   
+nums=[1,0,2,2,1,1,0]
+print(applyOperations(nums))
