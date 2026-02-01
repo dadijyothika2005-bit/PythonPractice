cd257756c6314776f9da16b4d0a32bb1a8332203
Dadi Jyothika <dadijyothika2005@gmail.com>
2026-02-01 20:10:47 +0530

    did operations on array and shifting zeroes to end using 2-point array

---
+++ LC2460.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def applyOperations(self, nums):
+        k=0
+        for i in range(len(nums)-1):
+            if(nums[i]==nums[i+1]):
+                nums[i]*=2
+                nums[i+1]=0
+        for i in range(len(nums)):
+            if(nums[i]!=0):
+                nums[k]=nums[i]
+                k+=1
+        for i in range(k,len(nums)):
+            nums[i]=0       
+        return nums   
